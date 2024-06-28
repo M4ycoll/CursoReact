929a6b38b2d6654840ee0cb1475551104511fc04
M4ycoll <maycollmoreno10@gmail.com>
2024-06-27 21:11:13 -0600

    React curso

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import {Testimonio}  from'./components/Testimonio.jsx';
 
+import testimoniofoto from '../public/Testimonio-foto.png';
+import testimonionino from '../public/Testimonio-nino.png';
+import testimoniopaisaje from '../public/Testimonio-paisaje.png';
 
 function App() {
   return (
@@ -10,11 +13,26 @@ function App() {
             <Testimonio
               nombre='Sara'
               pais='Honduras'
-              imagen=''
+              image={testimoniofoto} 
               cargo='Fotografa'
               empresa='Design'
               texto='Sint amet quis laboris Lorem. Amet Lorem nostrud qui minim. Ex amet fugiat officia incididunt irure dolor minim pariatur officia. Tempor ad aliquip reprehenderit fugiat duis. Incididunt irure culpa ipsum nostrud id quis est culpa aliquip pariatur nostrud nostrud. Tempor ea labore esse enim exercitation adipisicing voluptate consectetur laboris irure.'
-            
+            />
+            <Testimonio
+              nombre='Niño'
+              pais='Honduras'
+              image={testimonionino} 
+              cargo='Gamer'
+              empresa='Apple'
+              texto='Sint amet quis laboris Lorem. Amet Lorem nostrud qui minim. Ex amet fugiat officia incididunt irure dolor minim pariatur officia. Tempor ad aliquip reprehenderit fugiat duis. Incididunt irure culpa ipsum nostrud id quis est culpa aliquip pariatur nostrud nostrud. Tempor ea labore esse enim exercitation adipisicing voluptate consectetur laboris irure.'
+            />
+            <Testimonio
+              nombre='Paisaje'
+              pais='Italia'
+              image={testimoniopaisaje} 
+              cargo='Fotografo'
+              empresa='Desing'
+              texto='Sint amet quis laboris Lorem. Amet Lorem nostrud qui minim. Ex amet fugiat officia incididunt irure dolor minim pariatur officia. Tempor ad aliquip reprehenderit fugiat duis. Incididunt irure culpa ipsum nostrud id quis est culpa aliquip pariatur nostrud nostrud. Tempor ea labore esse enim exercitation adipisicing voluptate consectetur laboris irure.'
             />
           </div>
           
--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -5,10 +5,12 @@ import '../hojas-de-estilo/Testimonio.css'
 export function Testimonio(props){
     return(
         <div className='contenedor-testimonio'>
-            <img className="imagen-testimonio" src="../../public/Testimonio-foto.png" alt="foto-fotografa"/>
+            <img className="imagen-testimonio" 
+                 src={props.image} 
+                 alt="Testimonio foto" />
             <div className="contenedor-texto-testimonio">
-                <p className="nombre-testimonio">{props.nombre} de {props.pais}</p>
-                <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
+                <p className="nombre-testimonio"><strong>{props.nombre}</strong> de {props.pais}</p>
+                <p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong></p>
                 <p className="texto-testimonio">{props.texto}</p>
             </div>
         </div>
